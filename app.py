8bd2976b9225c148f4140a63175523ad35ec1508
Eng-Jinq-Kae <jinqkae17@gmail.com>
2026-01-11 02:08:58 +0800

    st app page link

--- app.py
+++ app.py
@@ -3,9 +3,23 @@ import pandas as pd
 import dataloader as dl
 from io import StringIO
 
+def page_link():
+    st.write("GOTO")
+    st.page_link(
+        "pages/1_Describe.py",
+        label="Go to Describe Page to know about your data",
+        icon=":material/eye_tracking:"
+    )
+    st.page_link(
+        "pages/2_Clustering.py",
+        label="Go to Clustering Page",
+        icon=":material/online_prediction:"
+    )
+
 if __name__ == "__main__":
     st.title("Nata Supermarkets: Customer Analytics")
     st.subheader("Upload your file here")
+    page_link()
 
     uploaded_file = st.file_uploader("Choose a file", type=["csv", "xlsx"])
     if uploaded_file is not None:
@@ -20,3 +34,6 @@ if __name__ == "__main__":
             df = st.session_state.file_upload
 
         st.dataframe(df.head())
+        page_link()
+    
+    st.success("Completely loaded.")
