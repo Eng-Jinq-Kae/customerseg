4cf70c45a219f688b9019a2d9585fe87674f4f24
Eng-Jinq-Kae <jinqkae17@gmail.com>
2026-01-11 02:01:03 +0800

    st1 describe data

---
+++ pages/1_Describe.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import dataloader as dl
+
+st.title("Describe")
+if 'file_upload' not in st.session_state or st.session_state.file_upload is None:
+    st.write("No file uploaded, Reading data from directory.")
+    df = dl.read_excel_file()
+else:
+    st.write("File uploaded is readed.")
+    df = st.session_state.file_upload
+
+st.dataframe(df.head())
