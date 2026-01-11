2883b2d95d56ac2f5480cc38d3c68a6bb08e8cae
Eng-Jinq-Kae <jinqkae17@gmail.com>
2026-01-12 03:03:56 +0800

    st1 call pipeline

--- pages/1_Describe.py
+++ pages/1_Describe.py
@@ -1,20 +1,6 @@
 import streamlit as st
 import dataloader as dl
 import pipeline as pipeline
-import altair as alt
-
-def chart_customer_age(df):
-    chart = (
-        alt.Chart(df)
-        .mark_bar()
-        .encode(
-            x=alt.X('Age:Q', bin=alt.Bin(maxbins=20), title='Age'),
-            y=alt.Y('count()', title='Number of Customers')
-        )
-        .properties(title='Customer Age Distribution')
-    )
-
-    st.altair_chart(chart, use_container_width=True)
 
 st.title("Describe")
 if 'file_upload' not in st.session_state or st.session_state.file_upload is None:
@@ -25,6 +11,5 @@ else:
     df = st.session_state.file_upload
 
 # st.dataframe(df.head())
-df = pipeline.add_customer_age(df)
-df = pipeline.impute_missing_salary(df)
-chart_customer_age(df)
+pipeline.st_describe(df)
+st.success("Describe page laoded full successfully.")
