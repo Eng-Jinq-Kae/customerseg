f828bc45d53b57356940cd7f842de6c4882c7d9d
Eng-Jinq-Kae <jinqkae17@gmail.com>
2026-01-12 02:04:46 +0800

    st1 histogram

--- pages/1_Describe.py
+++ pages/1_Describe.py
@@ -1,5 +1,20 @@
 import streamlit as st
 import dataloader as dl
+import pipeline as pipeline
+import altair as alt
+
+def chart_customer_age(df):
+    chart = (
+        alt.Chart(df)
+        .mark_bar()
+        .encode(
+            x=alt.X('Age:Q', bin=alt.Bin(maxbins=20), title='Age'),
+            y=alt.Y('count()', title='Number of Customers')
+        )
+        .properties(title='Customer Age Distribution')
+    )
+
+    st.altair_chart(chart, use_container_width=True)
 
 st.title("Describe")
 if 'file_upload' not in st.session_state or st.session_state.file_upload is None:
@@ -9,4 +24,7 @@ else:
     st.markdown(":material/check_circle: Reading uplaoded file.")
     df = st.session_state.file_upload
 
-st.dataframe(df.head())
+# st.dataframe(df.head())
+df = pipeline.add_customer_age(df)
+df = pipeline.impute_missing_salary(df)
+chart_customer_age(df)
